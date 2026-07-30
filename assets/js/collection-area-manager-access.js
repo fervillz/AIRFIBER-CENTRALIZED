caee7863fdedf6e29fbecfa747b630565fe73f9e
fernando villamor jr <fervillz@gmail.com>
2026-07-31 01:50:40 +0800

    Enable location manager after PPP users load

--- assets/js/collection-area-manager-access.js
+++ assets/js/collection-area-manager-access.js
@@ -9,11 +9,18 @@
 
 		const wrapper = document.createElement( 'div' );
 		wrapper.className = 'col-sm-auto';
-		wrapper.innerHTML = '<button class="btn btn-outline-primary" id="afc-manage-ppp-locations" type="button">Manage Locations</button>';
+		wrapper.innerHTML = '<button class="btn btn-outline-primary" id="afc-manage-ppp-locations" type="button" disabled>Manage Locations</button>';
 		printButton.closest( '.col-sm-auto' ).insertAdjacentElement( 'afterend', wrapper );
 
-		$( '#afc-manage-ppp-locations' ).on( 'click', function () {
+		const button = $( '#afc-manage-ppp-locations' );
+		button.on( 'click', function () {
 			document.dispatchEvent( new CustomEvent( 'afc:open-area-manager' ) );
+		} );
+
+		$( document ).ajaxSuccess( function ( event, xhr, settings ) {
+			if ( String( settings.data || '' ).includes( 'action=afc_get_ppp_users' ) && xhr.responseJSON && xhr.responseJSON.success ) {
+				button.prop( 'disabled', false );
+			}
 		} );
 	} );
 }( jQuery ) );
