d13dfb9fb01088a6250f8aa843d7c062dc0a8324
fernando villamor jr <fervillz@gmail.com>
2026-07-30 18:10:39 +0800

    Add PPP user workflow to airfiber-centralized.php

--- airfiber-centralized.php
+++ airfiber-centralized.php
@@ -2,25 +2,27 @@
 /**
  * Plugin Name: Airfiber - Centralized
  * Description: Customer, billing, payment, installation, notification, and MikroTik management for Airfiber.
- * Version: 0.4.1
+ * Version: 0.5.0
  * Author: Airfiber
  * Text Domain: airfiber-centralized
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AFC_VERSION', '0.4.1' );
+define( 'AFC_VERSION', '0.5.0' );
 define( 'AFC_FILE', __FILE__ );
 define( 'AFC_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AFC_URL', plugin_dir_url( __FILE__ ) );
 
 require_once AFC_PATH . 'includes/class-afc-post-types.php';
 require_once AFC_PATH . 'includes/class-afc-mikrotik.php';
+require_once AFC_PATH . 'includes/class-afc-ppp-users.php';
 require_once AFC_PATH . 'includes/class-afc-admin.php';
 
 function afc_boot_plugin() {
 	AFC_Post_Types::init();
 	AFC_MikroTik::init();
+	AFC_PPP_Users::init();
 
 	if ( is_admin() ) {
 		AFC_Admin::init();
