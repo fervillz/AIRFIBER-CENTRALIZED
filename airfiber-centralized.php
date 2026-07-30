31d1e7296d9b0eb484347531d74494540393eb6e
fernando villamor jr <fervillz@gmail.com>
2026-07-30 16:54:50 +0800

    Update airfiber-centralized.php for MikroTik settings

--- airfiber-centralized.php
+++ airfiber-centralized.php
@@ -2,23 +2,25 @@
 /**
  * Plugin Name: Airfiber - Centralized
  * Description: Customer, billing, payment, installation, notification, and MikroTik management for Airfiber.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Airfiber
  * Text Domain: airfiber-centralized
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AFC_VERSION', '0.1.0' );
+define( 'AFC_VERSION', '0.2.0' );
 define( 'AFC_FILE', __FILE__ );
 define( 'AFC_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AFC_URL', plugin_dir_url( __FILE__ ) );
 
 require_once AFC_PATH . 'includes/class-afc-post-types.php';
+require_once AFC_PATH . 'includes/class-afc-mikrotik.php';
 require_once AFC_PATH . 'includes/class-afc-admin.php';
 
 function afc_boot_plugin() {
 	AFC_Post_Types::init();
+	AFC_MikroTik::init();
 
 	if ( is_admin() ) {
 		AFC_Admin::init();
@@ -38,4 +40,3 @@ function afc_deactivate_plugin() {
 	flush_rewrite_rules();
 }
 register_deactivation_hook( __FILE__, 'afc_deactivate_plugin' );
-
