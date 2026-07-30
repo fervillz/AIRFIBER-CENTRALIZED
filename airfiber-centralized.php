7f649ca51d04851eaf7ffbc575f09533790e393b
fernando villamor jr <fervillz@gmail.com>
2026-07-30 17:53:08 +0800

    Add AJAX MikroTik testing to airfiber-centralized.php

--- airfiber-centralized.php
+++ airfiber-centralized.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Airfiber - Centralized
  * Description: Customer, billing, payment, installation, notification, and MikroTik management for Airfiber.
- * Version: 0.3.0
+ * Version: 0.4.0
  * Author: Airfiber
  * Text Domain: airfiber-centralized
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AFC_VERSION', '0.3.0' );
+define( 'AFC_VERSION', '0.4.0' );
 define( 'AFC_FILE', __FILE__ );
 define( 'AFC_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AFC_URL', plugin_dir_url( __FILE__ ) );
