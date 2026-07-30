559d9d740cca978318970ec81146f97ba3900421
fernando villamor jr <fervillz@gmail.com>
2026-07-30 16:41:16 +0800

    Add uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
+
+// Customer and payment records are intentionally preserved.
+// Permanent data removal will be added later as an explicit opt-in setting.
+
