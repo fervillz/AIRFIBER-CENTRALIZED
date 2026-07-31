f502b14c6eaaa9951ec6951b9607c680f07e7a77
fernando villamor jr <fervillz@gmail.com>
2026-07-31 11:05:23 +0800

    Release installable Airfiber PWA version 1.1.0

--- airfiber-centralized.php
+++ airfiber-centralized.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Airfiber - Centralized
  * Description: Customer, billing, payment, installation, notification, and MikroTik management for Airfiber.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Airfiber
  * Text Domain: airfiber-centralized
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AFC_VERSION', '1.0.0' );
+define( 'AFC_VERSION', '1.1.0' );
 define( 'AFC_FILE', __FILE__ );
 define( 'AFC_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AFC_URL', plugin_dir_url( __FILE__ ) );
@@ -25,6 +25,7 @@ require_once AFC_PATH . 'includes/class-afc-admin-mode.php';
 require_once AFC_PATH . 'includes/class-afc-basic-payments.php';
 require_once AFC_PATH . 'includes/class-afc-quick-payments.php';
 require_once AFC_PATH . 'includes/class-afc-frontend-page.php';
+require_once AFC_PATH . 'includes/class-afc-pwa.php';
 
 function afc_boot_plugin() {
 	AFC_Post_Types::init();
@@ -33,6 +34,7 @@ function afc_boot_plugin() {
 	AFC_Comment_Aliases::init();
 	AFC_Area_Manager::init();
 	AFC_Frontend_Page::init();
+	AFC_PWA::init();
 
 	if ( is_admin() ) {
 		AFC_Admin::init();
