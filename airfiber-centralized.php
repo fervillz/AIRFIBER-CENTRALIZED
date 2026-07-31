bd42e1ac3c8d445d30a0221b623978bc392253c7
fernando villamor jr <fervillz@gmail.com>
2026-07-31 15:06:09 +0800

    Register advanced comment fields manager

--- airfiber-centralized.php
+++ airfiber-centralized.php
@@ -2,20 +2,21 @@
 /**
  * Plugin Name: Airfiber - Centralized
  * Description: Customer, billing, payment, installation, notification, and MikroTik management for Airfiber.
- * Version: 1.1.3
+ * Version: 1.2.0
  * Author: Airfiber
  * Text Domain: airfiber-centralized
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AFC_VERSION', '1.1.3' );
+define( 'AFC_VERSION', '1.2.0' );
 define( 'AFC_FILE', __FILE__ );
 define( 'AFC_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AFC_URL', plugin_dir_url( __FILE__ ) );
 
 require_once AFC_PATH . 'includes/class-afc-post-types.php';
 require_once AFC_PATH . 'includes/class-afc-mikrotik.php';
+require_once AFC_PATH . 'includes/class-afc-comment-fields.php';
 require_once AFC_PATH . 'includes/class-afc-ppp-users.php';
 require_once AFC_PATH . 'includes/class-afc-comment-aliases.php';
 require_once AFC_PATH . 'includes/class-afc-area-manager.php';
@@ -30,6 +31,7 @@ require_once AFC_PATH . 'includes/class-afc-pwa.php';
 function afc_boot_plugin() {
 	AFC_Post_Types::init();
 	AFC_MikroTik::init();
+	AFC_Comment_Fields::init();
 	AFC_PPP_Users::init();
 	AFC_Comment_Aliases::init();
 	AFC_Area_Manager::init();
