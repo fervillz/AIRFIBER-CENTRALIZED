c05a0ed71ab0b91b9c2b52696c2abafbcf8fbd4b
fernando villamor jr <fervillz@gmail.com>
2026-07-31 20:36:03 +0800

    Release Comment Center mega-menu v1.5.0

--- airfiber-centralized.php
+++ airfiber-centralized.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Airfiber - Centralized
  * Description: Customer, billing, payment, installation, notification, and MikroTik management for Airfiber.
- * Version: 1.4.3
+ * Version: 1.5.0
  * Author: Airfiber
  * Text Domain: airfiber-centralized
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AFC_VERSION', '1.4.3' );
+define( 'AFC_VERSION', '1.5.0' );
 define( 'AFC_FILE', __FILE__ );
 define( 'AFC_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AFC_URL', plugin_dir_url( __FILE__ ) );
@@ -20,6 +20,7 @@ require_once AFC_PATH . 'includes/class-afc-comment-fields.php';
 require_once AFC_PATH . 'includes/class-afc-comment-formatting.php';
 require_once AFC_PATH . 'includes/class-afc-comment-migration.php';
 require_once AFC_PATH . 'includes/class-afc-comment-migration-rules.php';
+require_once AFC_PATH . 'includes/class-afc-comment-center.php';
 require_once AFC_PATH . 'includes/class-afc-ppp-users.php';
 require_once AFC_PATH . 'includes/class-afc-comment-aliases.php';
 require_once AFC_PATH . 'includes/class-afc-area-manager.php';
@@ -39,6 +40,7 @@ function afc_boot_plugin() {
 	AFC_Comment_Formatting::init();
 	AFC_Comment_Migration::init();
 	AFC_Comment_Migration_Rules::init();
+	AFC_Comment_Center::init();
 	AFC_PPP_Users::init();
 	AFC_Comment_Aliases::init();
 	AFC_Area_Manager::init();
