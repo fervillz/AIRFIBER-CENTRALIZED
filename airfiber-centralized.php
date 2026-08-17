a793b327ed582b36a6ef01e3700deabfddfdabdd
fernando villamor jr <fervillz@gmail.com>
2026-08-18 03:16:07 +0800

    Enable OLT resizable console sidebar

--- airfiber-centralized.php
+++ airfiber-centralized.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Airfiber - Centralized
  * Description: Customer, billing, payment, installation, notification, and MikroTik management for Airfiber.
- * Version: 2.13.4
+ * Version: 2.13.5
  * Author: Airfiber
  * Text Domain: airfiber-centralized
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AFC_VERSION', '2.13.4' );
+define( 'AFC_VERSION', '2.13.5' );
 define( 'AFC_FILE', __FILE__ );
 define( 'AFC_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AFC_URL', plugin_dir_url( __FILE__ ) );
@@ -25,6 +25,7 @@ require_once AFC_PATH . 'includes/class-afc-olt-mac-link.php';
 require_once AFC_PATH . 'includes/class-afc-olt-overview.php';
 require_once AFC_PATH . 'includes/class-afc-olt-manager.php';
 require_once AFC_PATH . 'includes/class-afc-olt-manager-frontend.php';
+require_once AFC_PATH . 'includes/class-afc-olt-manager-console-ui.php';
 require_once AFC_PATH . 'includes/class-afc-comment-fields.php';
 require_once AFC_PATH . 'includes/class-afc-comment-formatting.php';
 require_once AFC_PATH . 'includes/class-afc-comment-migration.php';
@@ -96,6 +97,7 @@ function afc_boot_plugin() {
 	AFC_OLT_Overview::init();
 	AFC_OLT_Manager::init();
 	AFC_OLT_Manager_Frontend::init();
+	AFC_OLT_Manager_Console_UI::init();
 	AFC_Comment_Fields::init();
 	AFC_Comment_Formatting::init();
 	AFC_Comment_Migration::init();
