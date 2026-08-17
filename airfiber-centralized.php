622848fac7a7100ee09ac80d2f1fbc8bd070b033
fernando villamor jr <fervillz@gmail.com>
2026-08-17 19:09:00 +0800

    Bump version for Basic RX dedupe

--- airfiber-centralized.php
+++ airfiber-centralized.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Airfiber - Centralized
  * Description: Customer, billing, payment, installation, notification, and MikroTik management for Airfiber.
- * Version: 2.12.7
+ * Version: 2.12.8
  * Author: Airfiber
  * Text Domain: airfiber-centralized
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AFC_VERSION', '2.12.7' );
+define( 'AFC_VERSION', '2.12.8' );
 define( 'AFC_FILE', __FILE__ );
 define( 'AFC_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AFC_URL', plugin_dir_url( __FILE__ ) );
