a78ca34c7b4d0427ec1157e375f52a763e53b9f3
fernando villamor jr <fervillz@gmail.com>
2026-08-22 16:52:49 +0800

    Route Basic add button through PPP or ONU chooser

--- includes/class-afc-ios-dialogs.php
+++ includes/class-afc-ios-dialogs.php
@@ -2,6 +2,8 @@
 
 defined( 'ABSPATH' ) || exit;
 
+require_once AFC_PATH . 'includes/class-afc-gpon-standalone.php';
+
 /**
  * Applies the shared iOS-style dialog surface and the minimal Basic Add PPP
  * wizard without changing the complete Advanced PPP editor.
@@ -9,6 +11,7 @@ defined( 'ABSPATH' ) || exit;
 class AFC_IOS_Dialogs {
 
 	public static function init() {
+		AFC_GPON_Standalone::init();
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_frontend_assets' ), 99 );
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_admin_assets' ), 99 );
 	}
@@ -37,10 +40,24 @@ class AFC_IOS_Dialogs {
 			AFC_VERSION
 		);
 
+		$add_menu_js = AFC_PATH . 'assets/js/basic-add-menu.js';
+		wp_enqueue_script(
+			'afc-basic-add-menu',
+			AFC_URL . 'assets/js/basic-add-menu.js',
+			array( 'jquery', 'afc-ppp-manager' ),
+			file_exists( $add_menu_js ) ? (string) filemtime( $add_menu_js ) : AFC_VERSION,
+			true
+		);
+		wp_localize_script(
+			'afc-basic-add-menu',
+			'afcGPONStandalone',
+			AFC_GPON_Standalone::frontend_config()
+		);
+
 		wp_enqueue_script(
 			'afc-basic-ppp-wizard',
 			AFC_URL . 'assets/js/basic-ppp-wizard.js',
-			array( 'jquery', 'afc-ppp-manager' ),
+			array( 'jquery', 'afc-ppp-manager', 'afc-basic-add-menu' ),
 			AFC_VERSION,
 			true
 		);
