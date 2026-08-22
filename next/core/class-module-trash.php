4a1732aa9a71375cf0836e4dfbf7c1794aba63af
fernando villamor jr <fervillz@gmail.com>
2026-08-23 04:47:19 +0800

    Add module trash state

---
+++ next/core/class-module-trash.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Airfiber\Next;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Stores the soft-delete state for installable Airfiber Next modules.
+ * MU/Core components are never trashable.
+ */
+class Module_Trash {
+	const OPTION_TRASH = 'afcn_trashed_modules_v1';
+
+	public static function all() {
+		$value = get_option( self::OPTION_TRASH, array() );
+		return is_array( $value ) ? $value : array();
+	}
+
+	public static function is_trashed( $module_id ) {
+		$module_id = sanitize_key( $module_id );
+		$trash     = self::all();
+		return ! empty( $trash[ $module_id ] );
+	}
+
+	public static function trash( $module_id ) {
+		$module_id = sanitize_key( $module_id );
+		$module    = Module_Registry::get( $module_id );
+		if ( ! $module ) {
+			return new \WP_Error( 'afcn_module_missing', __( 'Module not found.', 'airfiber-centralized' ) );
+		}
+		if ( ! empty( $module['system'] ) ) {
+			return new \WP_Error( 'afcn_mu_module', __( 'Core MU components cannot be moved to Trash.', 'airfiber-centralized' ) );
+		}
+		$trash               = self::all();
+		$trash[ $module_id ] = time();
+		update_option( self::OPTION_TRASH, $trash, false );
+		Audit_Log::record( 'module_trashed', $module_id );
+		return true;
+	}
+
+	public static function restore( $module_id ) {
+		$module_id = sanitize_key( $module_id );
+		$trash     = self::all();
+		if ( isset( $trash[ $module_id ] ) ) {
+			unset( $trash[ $module_id ] );
+			update_option( self::OPTION_TRASH, $trash, false );
+			Audit_Log::record( 'module_restored', $module_id );
+		}
+		return true;
+	}
+}
